4067af6b59029cf950bafadfa46185a01691d094
joao-gustavo-silva-lima <dev.joaogustavo.dev@gmail.com>
2026-07-26 08:50:18 -0300

    refactor(globals): simplify filename parsing with direct string replacement
    - Replaced redundant RegExp object construction with standard string replacement.
    - Streamlined extraction of relative filename from native __filename global.

--- globals.js
+++ globals.js
@@ -1,7 +1,7 @@
 console.log(__dirname);
 console.log(__filename);
 
-const onlyFileName = __filename.replace(new RegExp(__dirname + "/"), "");
+const onlyFileName = __filename.replace(__dirname + "/", "");
 
 console.log(onlyFileName);
 //console.log(process);
